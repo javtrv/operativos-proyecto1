2d97baf5a0304ab43adbfe4e897332f74e7c9def
mgmdi <14-10444@usb.ve>
2019-10-02 09:58:49 -0400

    fixed types

--- Process.java
+++ Process.java
@@ -3,33 +3,35 @@
  */
 public class Process {
 
-    private int pid;
-    private double tiempo_llegada;
-    private double tiempo_cpu;
+    private String pid;
+    private int tiempoLlegada;
+    private int tiempoCPU;
+    private int tiempoIO;
     private int prioridad;
-    private double v_runtime;
+    private int vRuntime;
 
 
-    public Process(int pid_, double tiempo_llegada_, double tiempo_cpu_, int prioridad_, double vruntime){
+    public Process(String pid_, int tiempo_llegada_, int tiempo_io_, int tiempo_cpu_, int prioridad_, int vRuntime){
         this.pid = pid_;
-        this.tiempo_llegada = tiempo_llegada_;
-        this.tiempo_cpu = tiempo_cpu_;
+        this.tiempoLlegada = tiempo_llegada_;
+        this.tiempoIO = tiempo_io_;
+        this.tiempoCPU = tiempo_cpu_;
         this.prioridad = prioridad_;
-        this.v_runtime = vruntime;
+        this.vRuntime = vRuntime;
     }
 
-    public int get_pid(){
+    public String get_pid(){
         return pid;
     }
 
 
-    public double get_tiempo_llegada(){
-        return tiempo_llegada;
+    public int get_tiempo_llegada(){
+        return tiempoLlegada;
     }
 
 
-    public double get_tiempo_cpu(){
-        return tiempo_cpu;
+    public int get_tiempo_cpu(){
+        return tiempoCPU;
     }
 
 
@@ -37,7 +39,11 @@ public class Process {
         return prioridad;
     }
 
-    public double get_vruntime(){
-        return v_runtime;
+    public int get_vruntime(){
+        return vRuntime;
+    }
+
+    public int get_tiempo_io(){
+        return tiempoIO;
     }
 }
