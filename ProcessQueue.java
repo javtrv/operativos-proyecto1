e6ae8e2d1021a7fffd59d267c3963df07865e88a
mgmdi <14-10444@usb.ve>
2019-10-02 11:48:51 -0400

    added methods to process queue (para despues agregarle monitores?)

---
+++ ProcessQueue.java
@@ -0,0 +1,64 @@
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.util.Iterator;
+  
+public class ProcessQueue implements Iterable<Process>
+{ 
+  private Queue<Process> queue;
+  public ProcessQueue(){
+    this.queue = new LinkedList<Process>();
+  }
+
+  public void add_process(Process p){
+    this.queue.add(p);
+  }
+
+  public Process delete_process(){
+    return this.queue.remove();
+  }
+
+  public int size(){
+    return this.queue.size();
+  }
+
+  public Iterator<Process> iterator() {
+    return this.queue.iterator();
+}
+  public static void main(String[] args) 
+  { 
+    
+
+    // inicializamos procesos
+    ProcessQueue q = new ProcessQueue();
+
+    Process proceso1 = new Process("1",68,62,1,8,59);
+    Process proceso2 = new Process("2",99,78,9,8,68);
+    Process proceso3 = new Process("3",67,85,4,8,47);
+    Process proceso4 = new Process("4",17,32,5,8,13);
+    Process proceso5 = new Process("5",72,40,8,8,88);
+
+    // Añadimos los procesos a la cola
+    q.add_process(proceso1);
+    q.add_process(proceso2);
+    q.add_process(proceso3);
+    q.add_process(proceso4);
+    q.add_process(proceso5);
+  
+    // Enseñamos los elementos de la cola
+    System.out.println("Elementos de la cola-"); 
+
+    for(Process p: q){
+        System.out.println(p.get_pid());
+      }
+  
+    // Para remover los elementos de la cola. 
+    Process removedele = q.delete_process(); 
+    System.out.println("removed element-" + removedele.get_pid()); 
+  
+    System.out.println(q); 
+  
+
+    int size = q.size(); 
+    System.out.println("Tamano de la cola-" + size); 
+  } 
+} 
