26fc3222930ced2ae4174fb35cfab2b759cef0f1
Javier <jmedina@darient>
2019-10-10 21:02:37 -0400

    Se agrego el hilo initSchedulerThreat que manda al arbol de listos, un proceso nuevo cada 5 segundos

--- Scheduler.java
+++ Scheduler.java
@@ -2,32 +2,49 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 
-public class Scheduler{
 
-    private RedBlackBST<Integer, Process> readyTree;
-    private ProcessQueue blockQueue;
-    private ArrayList<Process> newProcesses;
+
+class initSchedulerThread extends Thread{
+    @Override
+    public void run(){
+        // Load Processes to New Processes list
+        newProcesses = Parser.ParseToProcess("procesos1.json");
+
+        for (Process process: newProcesses) {
+            System.out.println(newProcesses);
+            try{
+                sleep(5000);
+                readyTree.put(p.get_vruntime(), p);
+            }catch(InterruptedException e){
+            }
+        }
+
+        // Load Processes to RBT 
+        
+        /*for (Process p : newProcesses) {
+            readyTree.put(p.get_vruntime(), p);
+        }
+        // Processes in RBT
+        StdOut.println(readyTree.keys());*/
+    }
+}
+
+class Scheduler{
+    public static RedBlackBST<Integer, Process> readyTree;
+    public static ProcessQueue blockQueue;
+    public static ArrayList<Process> newProcesses;
 
     public Scheduler(){
         
         //this.newProcesses = new ArrayList<Process>();
-        this.blockQueue = new ProcessQueue();
-        this.readyTree = new RedBlackBST<Integer, Process>();
+        blockQueue = new ProcessQueue();
+        readyTree = new RedBlackBST<Integer, Process>();
         
     }
-    public void initScheduler() {
+    public void initScheduler() { //Aqui corremos el hilo de iniciar el Scheduler
         
-        // Load Processes to New Processes list
-        this.newProcesses = Parser.ParseToProcess("procesos1.json");
-
-        // Load Processes to RBT 
-        
-        for (Process p : newProcesses) {
-            readyTree.put(p.get_vruntime(), p);
-        }
-        // Processes in RBT
-        StdOut.println(readyTree.keys());
-        
+        initSchedulerThread init = new initSchedulerThread(); 
+        init.start();
     }
 
     public void executeScheduler(){
