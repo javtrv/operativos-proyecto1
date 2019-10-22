e64714c2332fb9295ae0ebc9bd96cf5c96125563
wrancisco <opefran@gmail.com>
2019-10-22 17:37:29 -0400

    solucionar problemas de las dependencias de javafx

--- Scheduler/src/main/java/Scheduler/App.java
+++ Scheduler/src/main/java/Scheduler/App.java
@@ -11,21 +11,42 @@ import javafx.stage.Stage;
 /**
  * Hello world!
  */
-public final class App{
+public final class App extends Application{
     private App() {
     }
 
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
+
+        launch(args);
+
         Scheduler sc = new Scheduler();
         sc.executeScheduler();
 
-        /*Interface ic = new Interface();
-        ic.start();
-
-        System.out.println("guachiman");*/
+        System.out.println("guachiman");
     }
 }
